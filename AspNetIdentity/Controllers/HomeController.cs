c308730fa2a37b93a4a8ed6e72ea587ba1712ac0
Yunus Emre Cebe <yunusemrecebe@gmail.com>
2021-06-18 21:07:07 +0300

    Kullanıcı kayıt olma ile ilgili gerekli operasyonlar kodlandı

--- AspNetIdentity/Controllers/HomeController.cs
+++ AspNetIdentity/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AspNetIdentity.Models;
+using AspNetIdentity.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +11,51 @@ namespace AspNetIdentity.Controllers
 {
     public class HomeController : Controller
     {
+        public UserManager<AppUser> _userManager { get; }
+
+        public HomeController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult LogIn()
+        {
+            return View();
+        }
+
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(UserViewModel userViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser appUser = new AppUser();
+                appUser.UserName = userViewModel.UserName;
+                appUser.Email = userViewModel.Email;
+                appUser.PhoneNumber = userViewModel.PhoneNumber;
+
+                IdentityResult result = await _userManager.CreateAsync(appUser, userViewModel.Password);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("LogIn");
+                }
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError("", item.Description);
+                }
+            }
+
+            return View(userViewModel);
         }
     }
 }
---
+++ AspNetIdentity/ViewModels/UserViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetIdentity.ViewModels
+{
+    public class UserViewModel
+    {
+        [Required(ErrorMessage = "Kullanıcı ismi boş bırakılamaz!")]
+        [Display(Name = "Kullanıcı Adı")]
+        public string UserName { get; set; }
+
+        [Display(Name = "Telefon")]
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Email boş bırakılamaz!")]
+        [Display(Name = "Email")]
+        [EmailAddress(ErrorMessage = "Email adresi formatlarına uyunuz!")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Parola boş bırakılamaz!")]
+        [Display(Name = "Parola")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
