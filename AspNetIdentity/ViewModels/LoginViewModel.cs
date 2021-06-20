cc582a60a2075dedd49444aa6f3a6cb2b381c3bb
Yunus Emre Cebe <yunusemrecebe@gmail.com>
2021-06-20 22:09:12 +0300

    Login ekranı form tasarımı

---
+++ AspNetIdentity/ViewModels/LoginViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetIdentity.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Display(Name = "Email Adresi")]
+        [Required(ErrorMessage = "Email adresi boş bırakılamaz!")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Display(Name = "Parola")]
+        [Required(ErrorMessage = "Parola boş bırakılamaz!")]
+        [DataType(DataType.Password)]
+        [MinLength(3, ErrorMessage = "Parola en az 3 karakterden oluşmalıdır!")]
+        public string Password { get; set; }
+    }
+}
