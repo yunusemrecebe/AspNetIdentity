42097054b55571ccf38a91a874c9a0d97415eba5
Yunus emre Cebe <yunusemrecebe@gmail.com>
2021-06-27 18:16:13 +0300

    Üye anasayfası tasarlandı ve kodlandı

--- AspNetIdentity/Controllers/MemberController.cs
+++ AspNetIdentity/Controllers/MemberController.cs
@@ -1,18 +1,30 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using AspNetIdentity.Models;
+using AspNetIdentity.ViewModels;
+using Mapster;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace AspNetIdentity.Controllers
 {
     [Authorize]
     public class MemberController : Controller
     {
+        public UserManager<AppUser> _userManager { get; }
+        public SignInManager<AppUser> _signInManager { get; }
+
+        public MemberController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            AppUser user = _userManager.FindByNameAsync(User.Identity.Name).Result;
+            UserViewModel userViewModel = user.Adapt<UserViewModel>();
+
+            return View(userViewModel);
         }
     }
 }
